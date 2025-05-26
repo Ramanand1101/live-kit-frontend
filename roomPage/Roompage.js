1dbe4894ae63123ec91b525b1dd4aeba9775d014
Ramanand Tiwari <rntiwari383@gmail.com>
2025-05-26 14:11:12 +0530

    add the page

--- roomPage/Roompage.js
+++ roomPage/Roompage.js
@@ -46,7 +46,7 @@ export default function RoomPage() {
       roomRef.current = room;
 
       await room.connect(process.env.NEXT_PUBLIC_LIVEKIT_URL, token);
-      console.log(process.env.NEXT_PUBLIC_LIVEKIT_URL)
+      console.log("ssaaSAs",process.env.NEXT_PUBLIC_LIVEKIT_URL)
 
       if (isPublisher) {
         const localTracks = await createLocalTracks({ audio: true, video: true });
