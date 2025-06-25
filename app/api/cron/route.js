fbb6699e483e0daba1c012e2dec39e2b99020cfe
Ramanand Tiwari <rntiwari383@gmail.com>
2025-06-25 18:37:30 +0530

    new code added

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.log("✅ Cron job TRIGGERED at:", new Date().toISOString());
   return NextResponse.json({ ok: true });
 }
