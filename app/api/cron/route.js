07d13bd20fe2c7d3f7397089d81f2f5513d07904
Ramanand Tiwari <rntiwari383@gmail.com>
2025-06-25 19:01:19 +0530

    new code added again

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,6 +1,12 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  console.log("✅ Cron job TRIGGERED at:", new Date().toISOString());
+export async function GET(request) {
+  const auth = request.headers.get('Authorization');
+  if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  console.log("✅ CRON Triggered at:", new Date().toISOString());
+
   return NextResponse.json({ ok: true });
 }
